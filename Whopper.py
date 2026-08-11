e2641e43ca59095b27eb5ccab2bcc3ac3c484371
John Smith <rexcapra@proton.me>
2026-08-11 13:56:23 +0200

    rounding

--- Whopper.py
+++ Whopper.py
@@ -3,31 +3,29 @@ import torch.nn as nn
 import torch.optim as optim
 
 X = torch.tensor([
-    [0.0, 0.0],
-    [0.0, 1.0],
-    [1.0, 0.0],
+    [2.0, 3.0],
+    [3.0, 4.0],
+    [2.0, 1.0],
     [1.0, 1.0]
 ])
 
 y = torch.tensor([
-    [0.0],
-    [1.0],
-    [1.0],
-    [0.0],
+    [5.0],
+    [7.0],
+    [3.0],
+    [2.0],
 ])
 
 model = nn.Sequential(
     nn.Linear(2, 8),
     nn.ReLU(),
-    nn.Linear(8, 1),
-    nn.Sigmoid()
+    nn.Linear(8, 1)
 )
 
-loss_fn = nn.BCEWithLogitsLoss()
+loss_fn = nn.MSELoss()
 optimizer = optim.Adam(model.parameters(), lr=0.05)
 
-
-for epoch in range(101):
+for epoch in range(10001):
     y_pred = model(X)
     loss = loss_fn(y_pred, y)
 
@@ -35,12 +33,15 @@ for epoch in range(101):
     loss.backward()
     optimizer.step()
 
-    if epoch % 50 == 0:
+    if epoch % 1000 == 0:
         print(f"epoch {epoch}, loss {loss.item():.4f}")
 
-print("\nFinal predictions:")
-with torch.no_grad():
-    preds = model(X)
-    for i in range(len(X)):
-        print(f"{X[i].tolist()} → {preds[i].item():.3f}")
+f1 = float(input("Enter the first float: "))
+f2 = float(input("Enter the second float: "))
 
+a = torch.tensor([[f1, f2]])
+
+with torch.no_grad():
+    inpPred = model(a)
+
+print(round(inpPred.item(),1))
